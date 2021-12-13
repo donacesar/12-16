f672672116a2367f60e19d9a9c7f4d9e264cdceb
donacesar <donacesar@gmail.com>
2021-12-13 11:53:53 +0300

    task_5

---
+++ task_5.php
@@ -0,0 +1,119 @@
+<?php
+
+$arr = [
+
+    [
+        'whois' => 'Sunny A. (UI/UX Expert)',
+        'role' => 'Lead Author',
+        'img' => 'img/demo/authors/sunny.png',
+        'alt' => 'Sunny A.',
+        'a1_href' => 'https://twitter.com/@myplaneticket',
+        'a1_val' => '@myplaneticket',
+        'a2_href' => 'https://wrapbootstrap.com/user/myorange',
+        'a2_title' => 'Contact Sunny'
+    ],
+    [
+        'whois' => 'Jos K. (ASP.NET Developer)',
+        'role' => 'Partner &amp; Contributor',
+        'img' => 'img/demo/authors/josh.png',
+        'alt' => 'Jos K.',
+        'a1_href' => 'https://twitter.com/@atlantez',
+        'a1_val' => '@atlantez',
+        'a2_href' => 'https://wrapbootstrap.com/user/Walapa',
+        'a2_title' => 'Contact Jos'
+    ],
+    [
+        'whois' => 'Jovanni L. (PHP Developer)',
+        'role' => 'Partner &amp; Contributor',
+        'img' => 'img/demo/authors/jovanni.png',
+        'alt' => 'Jovanni Lo',
+        'a1_href' => 'https://twitter.com/@lodev09',
+        'a1_val' => '@lodev09',
+        'a2_href' => 'https://wrapbootstrap.com/user/lodev09',
+        'a2_title' => 'Contact Jovanni'
+    ],
+    [
+        'whois' => 'Roberto R. (Rails Developer)',
+        'role' => 'Partner &amp; Contributor',
+        'img' => 'img/demo/authors/roberto.png',
+        'alt' => 'Jovanni Lo',
+        'a1_href' => 'https://twitter.com/@sildur',
+        'a1_val' => '@sildur',
+        'a2_href' => 'https://wrapbootstrap.com/user/sildur',
+        'a2_title' => 'Contact Roberto'
+    ]
+
+];
+
+ ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>
+            Подготовительные задания к курсу
+        </title>
+        <meta name="description" content="Chartist.html">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+        <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+        <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+        <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+        <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+    </head>
+    <body class="mod-bg-1 mod-nav-link ">
+        <main id="js-page-content" role="main" class="page-content">
+            <div class="col-md-6">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Задание
+                        </h2>
+                        <div class="panel-toolbar">
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                        </div>
+                    </div>
+                    <div class="panel-container show">
+                        <div class="panel-content">
+                           <div class="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
+                            <?php foreach($arr as $item):?>
+                                <div class="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                                    <img src="<?=$item['img']; ?>" alt="<?=$item['alt']; ?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                                    <div class="ml-2 mr-3">
+                                        <h5 class="m-0">
+                                            <?=$item['whois']; ?>
+                                            <small class="m-0 fw-300">
+                                                <?=$item['role']; ?>
+                                            </small>
+                                         </h5>
+                                        <a href="<?=$item['a1_href']; ?>" class="text-info fs-sm" target="_blank"><?=$item['a1_val']; ?></a> -
+                                        <a href="<?=$item['a2_href']; ?>" class="text-info fs-sm" target="_blank" title="<?=$item['a2_title']; ?>"><i class="fal fa-envelope"></i></a>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+
+        <script src="js/vendors.bundle.js"></script>
+        <script src="js/app.bundle.js"></script>
+        <script>
+            // default list filter
+            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+            // custom response message
+            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+        </script>
+    </body>
+</html>
